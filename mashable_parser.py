7d2859b525068d35285850996ae547d0f9d27eed
belgaloo <johntrash.connor@gmail.com>
2016-11-14 16:11:20 +0100

    Add extract video function

--- mashable_parser.py
+++ mashable_parser.py
@@ -1,6 +1,11 @@
 class mashable_parser:
 
     def __init__(self):
+        pass
+
+    def extract_video(self,url):
+        videos = []
+        return videos
 
     def parse(self,url):
         print('mashable parser')
